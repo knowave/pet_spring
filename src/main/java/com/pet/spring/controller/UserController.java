6e62e5be2f4f13b4268050e0d242e53e0d12218d
knowave <kwaktaemin98@gmail.com>
2024-05-27 17:41:54 +0900

    Defined User Controller

--- src/main/java/com/pet/spring/controller/UserController.java
+++ src/main/java/com/pet/spring/controller/UserController.java
@@ -1,2 +1,22 @@
-package com.pet.spring.controller;public class UserController {
+package com.pet.spring.controller;
+
+import com.pet.spring.dto.UserDto;
+import com.pet.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+    private UserService userService;
+
+    @PostMapping("/users/join")
+    public ResponseEntity<String> join(@RequestBody UserDto.Request createUserDto) {
+        userService.createUser(createUserDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("SUCCESS!");
+    }
 }
