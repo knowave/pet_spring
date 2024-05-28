37321cb7f5fff2df2bedf8e19ffe085ae8a1668d
knowave <kwaktaemin98@gmail.com>
2024-05-28 17:08:48 +0900

    email validation

--- src/main/java/com/pet/spring/service/UserService.java
+++ src/main/java/com/pet/spring/service/UserService.java
@@ -15,10 +15,11 @@ public class UserService {
 
     @Transactional
     public void createUser(UserDto.Request createUserDto) {
-        if (userRepository.existByEmail(createUserDto.getEmail())) {
+        if (userRepository.existsByEmail(createUserDto.getEmail())) {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
         createUserDto.setPassword(encoder.encode(createUserDto.getPassword()));
         userRepository.save(createUserDto.toEntity());
     }
+
 }
