f462cbdbb8905452e60a051e120363436c9afbef
knowave <kwaktaemin98@gmail.com>
2024-05-31 13:42:51 +0900

    get user by id in controller

--- src/main/java/com/pet/spring/domain/user/controller/UserController.java
+++ src/main/java/com/pet/spring/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pet.spring.domain.user.controller;
 
 import com.pet.spring.domain.user.dto.UserDto;
+import com.pet.spring.domain.user.entity.User;
 import com.pet.spring.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,12 @@ public class UserController {
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserDto.Request createUserDto) {
         userService.createUser(createUserDto);
-        return ResponseEntity.ok().body("SUCCESS!");
+        return ResponseEntity.created().body("SUCCESS!");
+    }
+
+    @GetMapping("")
+    public ResponseEntity<User> getUserById(@RequestParam Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
